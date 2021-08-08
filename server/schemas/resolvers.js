7943f44979e3edea9e4c3d469130c4e380dd7ab0
samuel Odubamowo <wemsamuel@gmail.com>
2021-08-08 15:50:07 -0700

    server query and graphql issues are fixed

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,69 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models')
+const { signToken } = require('../utils/auth')
+
+
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'hello World!'
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+
+                return userData
+            }
+            throw new AuthenticationError('Not logged in');
         }
+    },
+    Mutation: {
+        addUser: async (parent, { email, password }) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        login: async (parent, args) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { user, token };
+        },
+
+        saveBook: async (parent, { content }, context) => {
+            if (context.user) {
+                const saveBooks = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { saveBooks: content } },
+                    { new: true }
+                )
+                return saveBooks;
+            }
+            throw new AuthenticationError('Must be logged in to add New book to list');
+
+        },
+
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const removeBooks = await User.findByIdAndUpdate(
+                    // from user
+                    { _id: context.user._id },
+                    // book id to remove from user
+                    { $pull: { saveBooks: { bookId: bookId } } },
+                    { new: true }
+                )
+                return removeBooks;
+            }
+            throw new AuthenticationError('Must be logged in to remove book');
+
+        }
+
     }
 };
 
--- server/server.js
+++ server/server.js
@@ -1,49 +1,32 @@
 const express = require('express');
-<<<<<<< HEAD
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express')
 const path = require('path');
 
-const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require('./schemas')
+const db = require('./config/connection');
 const { authMiddleware } = require ('./utils/auth');
 
-const db = require('./config/connection');
+// const routes = require('./routes');
 
-
-const app = express();
 const PORT = process.env.PORT || 3001;
-
-const server = new ApolloServer({ 
-  typeDefs, 
-  resolvers, 
-  context: authMiddleware 
-});
-
-server.applyMiddleware({ app });
-=======
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
-
 const app = express();
-const PORT = process.env.PORT || 3001;
->>>>>>> parent of 873c60c (graphQl server is up and running, going to test for queries now)
+const server = new ApolloServer({ typeDefs, resolvers, context: authMiddleware })
 
-app.use(express.urlencoded({ extended: true }));
+server.applyMiddleware({ app })
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-// app.get('*', (req, res) => {
-//  res.sendFile(path.join(__dirname, '../client/build/index.html'))
-// });
 
-<<<<<<< HEAD
-=======
-app.use(routes);
->>>>>>> parent of 873c60c (graphQl server is up and running, going to test for queries now)
+// app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`)
+    console.log(`use GraphQl at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
