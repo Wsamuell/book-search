0e1da3f2bd447d8261c138b5d8d333bdc995c726
samuel Odubamowo <wemsamuel@gmail.com>
2021-08-08 16:22:47 -0700

    updated app js with apollo provider

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,51 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($email: String!, $password: String!, $username: String!) {
+        addUser(email: $email, password: $password, username: $username) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($content: bookData!) {
+        saveBook(content: $content) {
+            _id
+            username
+            savedBooks {
+                bookId
+                title
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            savedBooks {
+                bookId
+                title
+            }
+        }
+    }
+`;
