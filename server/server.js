873c60c4e959f680c3577b79f06e4e93092b9ea9
samuel Odubamowo <wemsamuel@gmail.com>
2021-08-07 17:16:55 -0700

    graphQl server is up and running, going to test for queries now

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express')
 const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
-const routes = require('./routes');
+// const { authMiddleware } = require ('./utils/auth')
 
-const app = express();
+// const routes = require('./routes');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({ typeDefs, resolvers })
 
-app.use(express.urlencoded({ extended: true }));
+server.applyMiddleware({ app })
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -14,8 +22,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`)
+    console.log(`use GraphQl at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
